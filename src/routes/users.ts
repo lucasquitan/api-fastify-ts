f0dd9798159185872e0d6a67ae55ecfb791fe35c
Lucas Quintanilha <l.quitan@gmail.com>
2025-03-17 01:42:34 -0300

    add: Delete user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -37,12 +37,34 @@ export async function userRoutes(app: FastifyInstance) {
 
     if (cpf === undefined) {
       const users = await knex('users').select()
-      return users
+      return { total: users.length, users }
     }
     const user = await knex('users').where('cpf', cpf).first()
 
     if (user === undefined)
       return reply.status(404).send({ message: 'User not found' })
     return user
+  })
+
+  /**
+   * Delete a user by CPF or ID
+   */
+  app.delete('/:value', async (request, reply) => {
+    const deleteUserSchema = z.object({
+      value: z.string(),
+    })
+
+    const { value } = deleteUserSchema.parse(request.params)
+
+    const user = await knex('users')
+      .where('cpf', value)
+      .orWhere('id', value)
+      .first()
+
+    if (user === undefined)
+      return reply.status(404).send({ message: 'User not found' })
+
+    await knex('users').where('id', user.id).delete()
+    return reply.status(204).send()
   })
 }
