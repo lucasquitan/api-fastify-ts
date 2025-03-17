e84dbef87f058708cb5274f6e5d4784a58ec9775
Lucas Quintanilha <l.quitan@gmail.com>
2025-03-16 23:16:16 -0300

    add: Logger

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,15 @@ import { userRoutes } from './routes/users'
 
 const server = fastify()
 
+/**
+ * Add a simple hook to log all requests
+ */
+server.addHook('onResponse', async (request, reply) => {
+  console.log(
+    `${new Date().toUTCString()} | ${request.method} ${request.url} | FROM ${request.ip} | STATUS ${reply.statusCode}`,
+  )
+})
+
 server.register(userRoutes, { prefix: 'users' })
 
 server
